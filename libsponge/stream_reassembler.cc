35e202dd0bbd06588604a6e4d294a55721d5f848
123liuziming <448918299@qq.com>
2021-04-22 21:14:32 +0800

    lab1 finish, I think it is shit

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,84 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity), _unorderedItem(), _unorderedBytes(0) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    //能写入的长度
+    auto lengthWritable = std::min(_output.remaining_capacity(), data.size());
+    //如果正好能接上
+    if (index == _indexNow) {
+        auto bytesWritten = _output.write(data.substr(0, lengthWritable));
+        if (eof) {
+            _output.end_input();
+        }
+        if (bytesWritten == 0) {
+            return;
+        }
+        for (size_t i = 0; i < lengthWritable; ++i) {
+            if (_unorderedItem.count(index + i)) {
+                _unorderedItem.erase(index + i);
+                --_unorderedBytes;
+            }
+        }
+        _indexNow += bytesWritten;
+    }
+    //完全接不上，放在map里面
+    else if (index > _indexNow) {
+        for (size_t i = 0; i < lengthWritable; ++i) {
+            if (_unorderedItem.count(index + i) || _unorderedBytes >= _capacity) {
+                continue;
+            }
+            bool realEof = i == lengthWritable - 1 && eof;
+            _unorderedItem.insert(std::make_pair(index + i, std::make_pair(data[i], realEof)));
+            ++_unorderedBytes;
+        }
+        return;
+    }
+    //有一部分可以接上
+    else {
+        if (data.size() + index > _indexNow) {
+            for (size_t i = _indexNow - index; i < data.size(); ++i) {
+                if (_unorderedItem.count(index + i) || _unorderedBytes >= _capacity) {
+                    continue;
+                }
+                bool realEof = i == data.size() - 1 && eof;
+                _unorderedItem.insert(std::make_pair(index + i, std::make_pair(data[i], realEof)));
+                ++_unorderedBytes;
+            }
+        }
+        //被完全覆盖，不用管了
+        else {
+            return;
+        }
+    }
+
+    //补全
+    std::string fix;
+    while (_unorderedItem.count(_indexNow)) {
+        auto item = _unorderedItem[_indexNow];
+        _unorderedItem.erase(_indexNow);
+        --_unorderedBytes;
+        fix += item.first;
+        if (item.second) {
+            _output.end_input();
+        }
+        if (fix.length() >= _output.remaining_capacity()) {
+            break;
+        }
+        ++_indexNow;
+    }
+    _output.write(fix);
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
 
-bool StreamReassembler::empty() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    return _unorderedBytes;
+}
+
+bool StreamReassembler::empty() const {
+    return stream_out().buffer_empty();
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,6 +5,8 @@
 
 #include <cstdint>
 #include <string>
+#include <deque>
+#include <map>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
@@ -14,6 +16,13 @@ class StreamReassembler {
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+
+    //现在的index
+    size_t _indexNow = 0;
+    //无序的数据
+    std::map<int, std::pair<char, bool>> _unorderedItem;
+    //无序数据字节数
+    size_t _unorderedBytes;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
