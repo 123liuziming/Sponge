61115aa1a0540a83edfa850707ae10d12fcb37c3
123liuziming <448918299@qq.com>
2021-05-07 15:02:59 +0800

    graceful code

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,4 @@
 #include "wrapping_integers.hh"
-#include <iostream>
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -10,6 +9,10 @@ template <typename... Targs>
 void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
+
+inline uint64_t abs_diff(uint64_t a, uint64_t b) {
+    return (a > b ? a - b : b - a);
+}
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
@@ -29,10 +32,13 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    WrappingInt32 checkPointWrapped = wrap(checkpoint, isn);
-    auto diff = n - checkPointWrapped;
-    if (diff < 0 && (checkpoint < INT32_MAX && diff + checkpoint <= UINT64_MAX)) {
-        return diff + static_cast<uint32_t>(checkpoint);
-    }
-    return diff + checkpoint;
+    auto diff = n - isn;
+    const uint64_t mask = 1ul << 32;
+    uint64_t modLeft = checkpoint / mask * mask;
+    uint64_t modRight = (checkpoint / mask + 1) * mask;
+    auto ptLeft = modLeft >= INT32_MAX || modLeft + diff > INT64_MAX ? modLeft + diff : modLeft + static_cast<uint32_t>(diff);
+    auto ptRight = modRight >= INT32_MAX || modRight + diff >= INT64_MAX ? modRight + diff : modRight + static_cast<uint32_t>(diff);
+    const uint64_t disLeft = abs_diff(checkpoint, ptLeft);
+    const uint64_t disRight = abs_diff(checkpoint, ptRight);
+    return disLeft <= disRight ? ptLeft : ptRight;
 }
